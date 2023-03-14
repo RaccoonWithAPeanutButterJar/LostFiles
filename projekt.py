ea0a810677f72d9a4d9ae9b51471338a9074294e
Rico Akiro Kaio <Rico.akiro@gmail.com>
2023-03-14 19:13:15 +0100

    Added drive detection

--- projekt.py
+++ projekt.py
@@ -1,11 +1,17 @@
 import easygui
 import os
+import win32api
+#Mida
 otsitav = easygui.enterbox("What file are you looking for?")
-path = easygui.buttonbox("From where do you want to search?", "choose", ['C:', 'K:'])
+#Arvutis leiduvad kettad
+drives = win32api.GetLogicalDriveStrings()
+drives = drives.split('\000')[:-1]
+#Kust
+path = easygui.buttonbox("From where do you want to search from?", "Choose which drive", drives)
+#Faili leidmine
 def find_files(filename, search_path):
    result = []
-
-# Wlaking top-down from the root
+# Walking top-down from the root
    for root, dir, files in os.walk(search_path):
       if filename in files:
          result.append(os.path.join(root, filename))
