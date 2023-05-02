1d0cdf3788f12c6b0cecb0bc62f765287f0079e5
Rico Akiro Kaio <Rico.akiro@gmail.com>
2023-05-02 19:07:50 +0200

    Lemmikud feature

--- projekt_ümberkirjutatud.py
+++ projekt_ümberkirjutatud.py
@@ -23,6 +23,7 @@ def kaust(filepath):
             sg.Text("Folder: "),
             #filepath here
             sg.Input(default_text = filepath, size=(25, 1), enable_events=True, key="-FOLDER-"),
+            sg.Button("Lemmik", enable_events = True, size = (6,1), key = "-FAVOURITE-"),
             sg.FolderBrowse(),
             sg.Button("OK", enable_events = True, size = (3, 1), key = "-OK-"),
         ],
@@ -38,7 +39,6 @@ def kaust(filepath):
         [sg.Text(size=(40, 1), key="-TOUT-")],
     ]
 
-    #Layout for main window
     layout = [
         [
             sg.Column(file_list_column),
@@ -46,18 +46,16 @@ def kaust(filepath):
             sg.Column(file_open_column),
         ]
     ]
-    #Window
+
     window = sg.Window("Kausta sisu", layout, finalize = True)
-    #While loop for the main window
+
     while True:
         event, values = window.read()
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
-        #Folder field was filled in
         if event == "-OK-":
             folder = values["-FOLDER-"]
             try:
-                # Get list of files in folder
                 file_list = os.listdir(folder)
             except:
                 file_list = []
@@ -68,8 +66,12 @@ def kaust(filepath):
                 if os.path.isfile(os.path.join(folder, f))
             ]
             window["-FILE LIST-"].update(fnames)
+        #Vajutatakse "lemmik" nuppu
+        if event == "-FAVOURITE-":
+            with open("lemmikud.txt", "a") as lemmikud:
+                    lemmikud.write(filepath + '\n')
         # A file was chosen from the listbox
-        elif event == "-FILE LIST-":  
+        if event == "-FILE LIST-":  
             filename = os.path.join(
                 values["-FOLDER-"], values["-FILE LIST-"][0]
                 )
@@ -116,13 +118,23 @@ with open ("ajalugu.txt", "r+") as ajal:
     else:
         pass
 
+#Ajaloo failist ridade lugemine
 with open("ajalugu.txt", "r+") as ajalugutxt:
-    filepaths = []
+    filepaths_ajalugu = []
     for rida in ajalugutxt:
-        filepaths.append(rida)
+        filepaths_ajalugu.append(rida)
         fpath = [
                 f
-                for f in filepaths
+                for f in filepaths_ajalugu
+                ]
+#Lemmikud failist ridade lugemine
+with open("lemmikud.txt", "r+") as lemmikudtxt:
+    filepaths_lemmikud = []
+    for rida in lemmikudtxt:
+        filepaths_lemmikud.append(rida)
+        fpath2 = [
+                f
+                for f in filepaths_lemmikud
                 ]
 
 faili_otsimis_menüü = [
@@ -140,57 +152,71 @@ ajalugu = [
     [sg.Listbox(values=[], enable_events=True, size=(40, 20), key="-AJALUGU-")],
 ]
 
+lemmikud = [
+    [sg.Text("Lemmikud")],
+    [sg.Listbox(values=[], enable_events=True, size=(40, 20), key="-LEMMIKUD-")],
+]
+
 layout2 = [
         [
             sg.Column(ajalugu),
+            sg.Column(lemmikud),
             sg.VSeperator(),
             sg.Column(faili_otsimis_menüü),
         ]
 ]
 
 window = sg.Window('Faili otsing', layout2, finalize = True)
-if filepaths:
+if filepaths_ajalugu:
     window["-AJALUGU-"].update(fpath)
+if filepaths_lemmikud:
+    window["-LEMMIKUD-"].update(fpath2)
 while True:
     event, values = window.read()
     if event == sg.WINDOW_CLOSED or event == "Exit" or event == "-CANCEL-":
         break
+    #Kui valitakse ajaloo tulbast fail
     elif event == "-AJALUGU-":
         filepath = values["-AJALUGU-"][0]
         break
+    #Kui valitakse lemmikute tulbast väärtus
+    elif event == "-LEMMIKUD-":
+        filepath = values["-LEMMIKUD-"][0]
+        break
+    #Kui otsitakse faili
     elif event == "-OK-":
         if values["-VASTUS-"] == 'top dir':
             if os.path.isdir(values["-INPUT-"]):
                 filepath = values["-INPUT-"]
-                with open("ajalugu.txt", "a") as history:
-                    history.write(filepath + '\n')
+                with open("ajalugu.txt", "a") as ajalugu:
+                    ajalugu.write(filepath + '\n')
                 break
             else:
                 filepath = find_files(values["-INPUT-"], values["-COMBO-"])
-                with open("ajalugu.txt", "a") as history:
-                    history.write(filepath + '\n')
+                with open("ajalugu.txt", "a") as ajalugu:
+                    ajalugu.write(filepath + '\n')
                 break
         if values["-VASTUS-"] == 'bot dir':
             if os.path.isdir(values["-INPUT-"]):
                 filepath = values["-INPUT-"].split("/")[0] + "/" + values["-INPUT-"].split("/")[1]
-                with open("ajalugu.txt", "a") as history:
-                    history.write(filepath + '\n')
+                with open("ajalugu.txt", "a") as ajalugu:
+                    ajalugu.write(filepath + '\n')
                 break
             else:
                 filepath = find_files(values["-INPUT-"], values["-COMBO-"]).split("\\")[0] + "\\" + find_files(values["-INPUT-"], values["-COMBO-"]).split("\\")[1]
-                with open("ajalugu.txt", "a") as history:
-                    history.write(filepath + '\n')
+                with open("ajalugu.txt", "a") as ajalugu:
+                    ajalugu.write(filepath + '\n')
                 break
         if values["-VASTUS-"] == 'ketas':
             if os.path.isdir(values["-INPUT-"]):
                 filepath = values["-INPUT-"][0:3]
-                with open("ajalugu.txt", "a") as history:
-                    history.write(filepath + '\n')
+                with open("ajalugu.txt", "a") as ajalugu:
+                    ajalugu.write(filepath + '\n')
                 break
             else:
                 filepath = find_files(values["-INPUT-"], values["-COMBO-"])[0:3]
-                with open("ajalugu.txt", "a") as history:
-                    history.write(filepath + '\n')
+                with open("ajalugu.txt", "a") as ajalugu:
+                    ajalugu.write(filepath + '\n')
                 break
 window.close()
 
