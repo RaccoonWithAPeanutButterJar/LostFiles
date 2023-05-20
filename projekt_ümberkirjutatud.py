7d9908fbcc9c405a971e16cb1e33a00131a34747
Rico Akiro Kaio <Rico.akiro@gmail.com>
2023-05-20 21:16:29 +0200

    LÕPETATUD?!

--- projekt_ümberkirjutatud.py
+++ projekt_ümberkirjutatud.py
@@ -3,47 +3,53 @@ from pathlib import Path
 import os
 import win32api
 
-global filepath
-global find_files
+global faili_asukoht
+global otsi_fail
 
 #Arvutis leiduvate ketaste leidmine
-global drives
-drives = win32api.GetLogicalDriveStrings()
-drives = drives.split('\000')[:-1]
-def add_drives(list = []):
-    for i in drives:
+global kettad
+kettad = win32api.GetLogicalDriveStrings()
+kettad = kettad.split('\000')[:-1]
+def lisa_kettad(list = []):
+    for i in kettad:
         list.append(i)
     return list
 
+#Popup otsingu jaoks
+def popup(sõnum):
+    layout = [[sg.Text(sõnum)]]
+    window = sg.Window('Sõnum', layout, no_titlebar=True, keep_on_top=True, finalize=True)
+    return window
+
 #Kausta minemine
-def kaust(filepath):
-    #File view
-    file_list_column = [
+def kaust(faili_asukoht):
+    #Faili vaade
+    Failide_tulp = [
         [
-            sg.Text("Folder: "),
-            #filepath here
-            sg.Input(default_text = filepath, size=(25, 1), enable_events=True, key="-FOLDER-"),
-            sg.Button("Lemmik", enable_events = True, size = (6,1), key = "-FAVOURITE-"),
+            sg.Text("Kaust: "),
+            #faili_asukoht here
+            sg.Input(default_text = faili_asukoht, size=(25, 1), enable_events=True, key="-KAUST-"),
+            sg.Button("Lemmik", enable_events = True, size = (6,1), key = "-LEMMIK-"),
             sg.FolderBrowse(),
             sg.Button("OK", enable_events = True, size = (3, 1), key = "-OK-"),
         ],
         [
             sg.Listbox(
-                values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
+                values=[], enable_events=True, size=(40, 20), key="-NIMEKIRI-"
             )
         ],
     ]
 
-    file_open_column = [
-        [sg.Text("Choose a file from the list:")],
+    Failide_valiku_tulp = [
+        [sg.Text("Vali fail nimekirjast:")],
         [sg.Text(size=(40, 1), key="-TOUT-")],
     ]
 
     layout = [
         [
-            sg.Column(file_list_column),
+            sg.Column(Failide_tulp),
             sg.VSeperator(),
-            sg.Column(file_open_column),
+            sg.Column(Failide_valiku_tulp),
         ]
     ]
 
@@ -54,53 +60,54 @@ def kaust(filepath):
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
         if event == "-OK-":
-            folder = values["-FOLDER-"]
+            kaust = values["-KAUST-"]
             try:
-                file_list = os.listdir(folder)
+                failide_nimekiri = os.listdir(kaust)
             except:
-                file_list = []
+                failide_nimekiri = []
 
             fnames = [
                 f
-                for f in file_list
-                if os.path.isfile(os.path.join(folder, f))
+                for f in failide_nimekiri
+                if os.path.isfile(os.path.join(kaust, f))
             ]
-            window["-FILE LIST-"].update(fnames)
+            window["-NIMEKIRI-"].update(fnames)
         #Vajutatakse "lemmik" nuppu
-        if event == "-FAVOURITE-":
+        if event == "-LEMMIK-":
             with open("lemmikud.txt", "a") as lemmikud:
-                    lemmikud.write(filepath + '\n')
-        # A file was chosen from the listbox
-        if event == "-FILE LIST-":  
-            filename = os.path.join(
-                values["-FOLDER-"], values["-FILE LIST-"][0]
+                    lemmikud.write(faili_asukoht + '\n')
+            sg.popup("Lisati lemmikutesse")
+        # Fail valiti nimekirjast
+        if event == "-NIMEKIRI-":  
+            faili_nimi = os.path.join(
+                values["-KAUST-"], values["-NIMEKIRI-"][0]
                 )
-            if Path(filename).is_file():
+            if Path(faili_nimi).is_file():
                 try:
-                    with open(filename, "rt", encoding='utf-8') as f:
+                    with open(faili_nimi, "rt", encoding='utf-8') as f:
                         text = f.read()
-                    popup_text(filename, text)
+                    popup_text(faili_nimi, text)
                 except Exception as e:
                     print("Error: ", e)
         
     window.close()
 
 #Faili leidmine
-def find_files(filename, search_path):
-   result = []
+def otsi_fail(faili_nimi, rada):
+   tulemus = []
 # Walking top-down from the root
-   for root, dir, files in os.walk(search_path):
-      if filename in files:
-         result.append(os.path.join(root, filename))
-   return result[0]
+   for root, dir, failid in os.walk(rada):
+      if faili_nimi in failid:
+         tulemus.append(os.path.join(root, faili_nimi))
+   return tulemus[0]
 
-#definition for popup
-def popup_text(filename, text):
+#kui avada tekstifail
+def popup_text(faili_nimi, text):
 
     layout = [
         [sg.Multiline(text, size=(80, 25)),],
     ]
-    win = sg.Window(filename, layout, modal=True, finalize=True)
+    win = sg.Window(faili_nimi, layout, modal=True, finalize=True)
 
     while True:
         event, values = win.read()
@@ -108,6 +115,7 @@ def popup_text(filename, text):
             break
     win.close()  
 
+#Kui failis "ajalugu" on rohkem kui 20 asja, siis kustutab algusest esimese ära
 with open ("ajalugu.txt", "r+") as ajal:
     read = ajal.readlines()
     if len(read) > 20:
@@ -120,38 +128,40 @@ with open ("ajalugu.txt", "r+") as ajal:
 
 #Ajaloo failist ridade lugemine
 with open("ajalugu.txt", "r+") as ajalugutxt:
-    filepaths_ajalugu = []
+    faili_asukoht_ajalugu = []
     for rida in ajalugutxt:
-        filepaths_ajalugu.append(rida)
+        faili_asukoht_ajalugu.append(rida)
         fpath = [
                 f
-                for f in filepaths_ajalugu
+                for f in faili_asukoht_ajalugu
                 ]
 #Lemmikud failist ridade lugemine
 with open("lemmikud.txt", "r+") as lemmikudtxt:
-    filepaths_lemmikud = []
+    faili_asukoht_lemmikud = []
     for rida in lemmikudtxt:
-        filepaths_lemmikud.append(rida)
+        faili_asukoht_lemmikud.append(rida)
         fpath2 = [
                 f
-                for f in filepaths_lemmikud
+                for f in faili_asukoht_lemmikud
                 ]
 
 faili_otsimis_menüü = [
         [sg.Text('Mis faili soovid leida: ')],
         [sg.In(size=(40, 20), key = "-INPUT-"), sg.FolderBrowse()],
         [sg.Text('Kust kettalt (Kui kasutad browse, siis võid vahele jätta): ')],
-        [sg.Combo(values = add_drives(), default_value = 'C:\\', font=('Arial Bold', 14), expand_x=True, enable_events=True,  readonly=False, key='-COMBO-')],
+        [sg.Combo(values = lisa_kettad(), default_value = 'C:\\', font=('Arial Bold', 14), expand_x=True, enable_events=True,  readonly=False, key='-KOMBO-')],
         [sg.Text('Missugust vastuse formaati soovid: ')],
         [sg.Combo(values = ['top dir', 'bot dir', 'ketas'], default_value = 'top dir', font=('Arial Bold', 14), expand_x=True, enable_events=True,  readonly=False, key='-VASTUS-')],
         [sg.Button("OK", enable_events = True, size = (3, 1), key = "-OK-"), sg.Button("Cancel", enable_events = True, key = "-CANCEL-")]
         ]
 
+#Ajaloo kast
 ajalugu = [
     [sg.Text("Ajalugu")],
     [sg.Listbox(values=[], enable_events=True, size=(40, 20), key="-AJALUGU-")],
 ]
 
+#Lemmikute kast
 lemmikud = [
     [sg.Text("Lemmikud")],
     [sg.Listbox(values=[], enable_events=True, size=(40, 20), key="-LEMMIKUD-")],
@@ -167,10 +177,11 @@ layout2 = [
 ]
 
 window = sg.Window('Faili otsing', layout2, finalize = True)
+popup_win = None
 
-if filepaths_ajalugu:
+if faili_asukoht_ajalugu:
     window["-AJALUGU-"].update(fpath)
-if filepaths_lemmikud:
+if faili_asukoht_lemmikud:
     window["-LEMMIKUD-"].update(fpath2)
 while True:
     event, values = window.read()
@@ -178,55 +189,65 @@ while True:
         break
     #Kui valitakse ajaloo tulbast fail
     elif event == "-AJALUGU-":
-        filepath = values["-AJALUGU-"][0]
+        faili_asukoht = values["-AJALUGU-"][0]
         break
     #Kui valitakse lemmikute tulbast väärtus
     elif event == "-LEMMIKUD-":
-        filepath = values["-LEMMIKUD-"][0]
+        faili_asukoht = values["-LEMMIKUD-"][0]
         break
     #Kui otsitakse faili
     elif event == "-OK-":
+        window['-OK-'].update(disabled=True)
+        popup_win = popup('Otsin...')
+        window.force_focus()
         if values["-VASTUS-"] == 'top dir':
             if os.path.isdir(values["-INPUT-"]):
-                filepath = values["-INPUT-"]
+                faili_asukoht = values["-INPUT-"]
                 with open("ajalugu.txt", "a") as ajalugu:
-                    ajalugu.write(filepath + '\n')
+                    ajalugu.write(faili_asukoht + '\n')
                 break
             else:
-                filepath = find_files(values["-INPUT-"], values["-COMBO-"])
+                faili_asukoht = otsi_fail(values["-INPUT-"], values["-KOMBO-"])
                 with open("ajalugu.txt", "a") as ajalugu:
-                    ajalugu.write(filepath + '\n')
+                    ajalugu.write(faili_asukoht + '\n')
                 break
         if values["-VASTUS-"] == 'bot dir':
             if os.path.isdir(values["-INPUT-"]):
-                filepath = values["-INPUT-"].split("/")[0] + "/" + values["-INPUT-"].split("/")[1]
+                faili_asukoht = values["-INPUT-"].split("/")[0] + "/" + values["-INPUT-"].split("/")[1]
                 with open("ajalugu.txt", "a") as ajalugu:
-                    ajalugu.write(filepath + '\n')
+                    ajalugu.write(faili_asukoht + '\n')
                 break
             else:
-                filepath = find_files(values["-INPUT-"], values["-COMBO-"]).split("\\")[0] + "\\" + find_files(values["-INPUT-"], values["-COMBO-"]).split("\\")[1]
+                faili_asukoht = otsi_fail(values["-INPUT-"], values["-KOMBO-"]).split("\\")[0] + "\\" + otsi_fail(values["-INPUT-"], values["-KOMBO-"]).split("\\")[1]
                 with open("ajalugu.txt", "a") as ajalugu:
-                    ajalugu.write(filepath + '\n')
+                    ajalugu.write(faili_asukoht + '\n')
                 break
         if values["-VASTUS-"] == 'ketas':
             if os.path.isdir(values["-INPUT-"]):
-                filepath = values["-INPUT-"][0:3]
+                faili_asukoht = values["-INPUT-"][0:3]
                 with open("ajalugu.txt", "a") as ajalugu:
-                    ajalugu.write(filepath + '\n')
+                    ajalugu.write(faili_asukoht + '\n')
                 break
             else:
-                filepath = find_files(values["-INPUT-"], values["-COMBO-"])[0:3]
+                faili_asukoht = otsi_fail(values["-INPUT-"], values["-KOMBO-"])[0:3]
                 with open("ajalugu.txt", "a") as ajalugu:
-                    ajalugu.write(filepath + '\n')
+                    ajalugu.write(faili_asukoht + '\n')
                 break
+        popup_win.close()
+        popup_win = None
+        window['-OK-'].update(disabled=False)
 window.close()
 
-if os.path.isdir(filepath):
-   kaust(filepath)
+if popup_win:
+    popup_win.close()
+window.close()
+
+if os.path.isdir(faili_asukoht):
+   kaust(faili_asukoht)
 else:
     faili_tulemuste_menüü = [
         [
-            sg.Text(f"Sinu fail asub: {filepath}"),
+            sg.Text(f"Sinu fail asub: {faili_asukoht}"),
             sg.Button("Vii sinna", enable_events = True, key = "-VII-"),
         ]
     ]
@@ -238,7 +259,7 @@ else:
         if event == sg.WINDOW_CLOSED or event == "Exit" or event == "-CANCEL-":
             break
         elif event == "-VII-":
-            kaust(filepath[0:filepath.rfind("\\")])
+            kaust(faili_asukoht[0:faili_asukoht.rfind("\\")])
             break
             
     window.close()
